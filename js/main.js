21ba0ba39456229c556705a272f6b5bd2eb25993
RangiBecerra <rangibecerra@gmail.com>
2017-05-16 19:57:14 -0400

    funciones terminadas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-alert("existe");
+function pagina() {
+    alert("La página está cargada")
+}
+
+function out() {
+    alert("Estuvistes sobre MI")
+}
+
+function over() {
+    alert("Estás sobre MI")
+}
+
+var saludo = function() {
+    alert("HOLA MUNDO");
+};
+
+function principal() {
+    var btn = document.getElementById("demo");
+    btn.addEventListener("click", saludo);
+};
+
+principal();
